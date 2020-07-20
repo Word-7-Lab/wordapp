b217667ee6d6b516b0750611b749b176805d8301
Word-7-lab <word.7.lab@gmail.com>
2020-07-19 22:20:53 -0400

    Updates

---
+++ precache-manifest.e4c5c64b7f4deb6e4a04bc9934b68db8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c6ae8c6d7c1b1b2b42f2752df6d54b1",
+    "url": "/Word-7-Lab/wordapp.git/index.html"
+  },
+  {
+    "revision": "64c69e2c1a2adbe94c1e",
+    "url": "/Word-7-Lab/wordapp.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fe20d6792ffb12419763",
+    "url": "/Word-7-Lab/wordapp.git/static/js/2.83e7432e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Word-7-Lab/wordapp.git/static/js/2.83e7432e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64c69e2c1a2adbe94c1e",
+    "url": "/Word-7-Lab/wordapp.git/static/js/main.e51bd044.chunk.js"
+  },
+  {
+    "revision": "bbdac8e12f3492654a13",
+    "url": "/Word-7-Lab/wordapp.git/static/js/runtime-main.5725dca0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Word-7-Lab/wordapp.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
