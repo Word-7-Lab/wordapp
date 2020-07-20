b3aee52e4e93dfbad83ac2a2818ccc20569785c6
Word-7-lab <word.7.lab@gmail.com>
2020-07-19 23:58:10 -0400

    third commit

--- src/App.js
+++ src/App.js
@@ -2,19 +2,57 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Navigation from './components/Navigation';
+import { todos } from './todos.json';
+
 
 class App extends Component {
-  render(){
+  constructor() {
+    super();
+    this.state = {
+      todos
+    };
+  }
+
+
+  render() {
+
+    const todos = this.state.todos.map((todo, i) => {
+      return (
+        <div className="col-md-4">
+          <div className="card mt-4">
+            <div className="card-header">
+              <h3>{todo.title}</h3>
+              <span className="badge badge-pill badge-danger ml-2">
+                {todo.priority}
+              </span>
+            </div>
+            <div className="card-body">
+              <p>{todo.description}</p>
+              <p><mark>{todo.responsible}</mark></p>
+            </div>
+          </div>
+        </div>
+      );
+    });
+
     return (
       <div className="App">
-        <Navigation titulo="Mi primera Nav" />
-        <Navigation titulo="Mi segunda Nav" />
+        <nav className="navbar navbar-dark bg-dark">
+          <a href="#" className="text-white">
+            Tasks
+             </a>
+        </nav>
+
+        <div className="container">
+          <div className="row mt-4">
+             {todos}
+          </div>
+        </div>
 
         <img src={logo} className="App-logo" alt="logo" />
       </div>
     );
-  }  
+  }
 }
 
 export default App;
